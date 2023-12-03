3f90b0346df49959899cee6b802055d0fb383c4d
Runfeng Li <runfeng_li@brown.edu>
2023-12-03 00:37:33 -0500

    Implement Jacobi method with residual calculation (not tested)

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Attenuation map of the Belgium House scene.
 
 ### References
 Fattal, R., Lischinski, D., & Werman, M. (2023). _Gradient domain high dynamic range compression._ In Seminal Graphics Papers: Pushing the Boundaries, Volume 2 (pp. 671-678).
+
+### Problems
+1. The poisson solver, if using the residual to stop iterating, can only deal with images with fewer than 2^30 pixels for now, and the implementation for calculating residual/error between previous and the current results may be efficient.
--- poisson_solver/solver.h
+++ poisson_solver/solver.h
@@ -2,18 +2,62 @@
 #define POISSON_SOLVER_H_INCLUDED
 
 #include <cuda_runtime.h>
-// #include <iostream>
+#include <device_launch_parameters.h>
 
+/*
+    Jacobi iterative method:
+        x_i^{k+1} = \frac{1}{-4} (divG_i - x_{i-1}^k - x_{i-W}^k - x_{i+1}^k - x_{i+W}^k).
+*/
 void jacobiSolver(
     const int H, const int W, 
-    const float* divG,
-    const int iterations, const float tolerence,
-    float* I_log
+    const float* d_divG,
+    const int iterations, const float tolerance, const int checkFrequency,
+    float* d_I_log
 );
 
+/*
+    Gauss-Seidel iterative method:
+        x_i^{k+1} = \frac{1}{-4} (divG_i - x_{i-1}^{k+1} - x_{i-W}^{k+1} - x_{i+1}^k - x_{i+W}^k).
+*/
+void gaussSeidelSolver(
+    const int H, const int W, 
+    const float* d_divG,
+    const int iterations, const float tolerance, const int checkFrequency,
+    float* d_I_log
+);
+
+/*
+    Gauss-Seidel iterative method with Red-Black Reordering:
+        x_{2i}^{k+1} = \frac{1}{-4} (divG_i - x_{i-1}^k - x_{i-W}^k - x_{i+1}^k - x_{i+W}^k),
+        x_{2i+1}^{k+1} = \frac{1}{-4} (divG_i - x_{i-1}^{k+1} - x_{i-W}^{k+1} - x_{i+1}^{k+1} - x_{i+W}^{k+1}).
+*/
+void gaussSeidelRedBlackSolver(
+    const int H, const int W, 
+    const float* d_divG,
+    const int iterations, const float tolerance, const int checkFrequency,
+    float* d_I_log
+);
+
+/*
+    Gauss-Seidel iterative method with Red-Black Reordering and Overrelaxation (SOR):
+        x_{2i}^{k+1} = \frac{1}{-4} (divG_i - w_{opt} * x_{i-1}^k - w_{opt} * x_{i-W}^k - x_{i+1}^k - x_{i+W}^k),
+        x_{2i+1}^{k+1} = \frac{1}{-4} (divG_i - w_{opt} * x_{i-1}^{k+1} - w_{opt} * x_{i-W}^{k+1} - x_{i+1}^{k+1} - x_{i+W}^{k+1}),
+        w_{opt} = \frac{2}{1 + \sqrt{1 - \cos{\pi/\frac{H+W}{2}}^2}}.
+*/
+void gaussSeidelRedBlackSORSolver(
+    const int H, const int W, 
+    const float* d_divG,
+    const int iterations, const float tolerance, const int checkFrequency,
+    float* d_I_log
+);
+
+/*
+    Full Multigrid Poisson Solver with Gauss-Seidel smoothing iteration + Red-Black Reordering and Overrelaxation (SOR).
+*/
 void fullMultigridSolver(
     const int H, const int W, 
-    const float* divG, 
-    float* I_log);
+    const float* d_divG, 
+    const int iterations, const float tolerance, const int checkFrequency,
+    float* d_I_log);
 
 #endif
